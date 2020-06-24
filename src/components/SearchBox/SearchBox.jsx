4b9a41aa39c478f3fc07b9c76ac4ccbb364bec06
ishvar99 <ishanvarshney99@gmail.com>
2020-06-24 14:16:46 +0530

    created search box

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './SearchBox.css';
 const SearchBox = () => {
-  return <div className='search'></div>;
+  return (
+    <div class='search-box'>
+      <input className='search-bar' type='text' placeholder='Search a number' />
+      <button className='fa fa-search' id='search-button'></button>
+    </div>
+  );
 };
 
 export default SearchBox;
