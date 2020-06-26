71b82a78c7c7f140336ef0a4f1a72e9999b1dfb0
ishvar99 <ishanvarshney99@gmail.com>
2020-06-27 00:02:45 +0530

    added set timeout for delay effect

--- src/components/ArrayItem/ArrayItem.jsx
+++ src/components/ArrayItem/ArrayItem.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ArrayItem.css';
+import ClassNames from 'classnames';
 const ArrayItem = ({ index, array, modifiedArray }) => {
-  // console.log(modifiedArray);
-  let className = 'array-item';
-
-  if (modifiedArray.length > 0) {
-    if (modifiedArray.some((e) => e.index === index && e.traversed === true)) {
-      className += ' traversed';
-    }
-    if (
-      modifiedArray.some(
-        (e) => e.index === index && e.traversed === true && e.result === true
-      )
-    ) {
-      className += ' scale';
-    }
-    if (
-      modifiedArray.some(
-        (e) => e.index === index && e.traversed === false && e.result === true
-      )
-    ) {
-      className += ' scale result';
-    }
-  }
+  const [className, setClassName] = useState(['array-item']);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (modifiedArray.length > 0) {
+        if (
+          modifiedArray.some((e) => e.index === index && e.traversed === true)
+        ) {
+          setClassName([...className, 'traversed']);
+        }
+        if (
+          modifiedArray.some(
+            (e) =>
+              e.index === index && e.traversed === true && e.result === true
+          )
+        ) {
+          setClassName([...className, 'scale']);
+        }
+        if (
+          modifiedArray.some(
+            (e) =>
+              e.index === index && e.traversed === false && e.result === true
+          )
+        ) {
+          console.log('result', index);
+          setClassName([...className, 'scale', 'result']);
+        }
+      }
+    }, 500 * index);
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [index, modifiedArray]);
   return (
-    <div className={className}>
+    <div className={ClassNames([...new Set(className)])}>
       <p>{array[index]}</p>
+      <div className='index'>
+        <p>{index}</p>
+      </div>
     </div>
   );
 };
